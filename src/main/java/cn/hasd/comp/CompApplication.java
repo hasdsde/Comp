b92150a440a43a4423ec6dcd64b70b3d500536a7
hasdsde <1259305706@qq.com>
2022-10-19 15:56:47 +0800

    新建项目

---
+++ src/main/java/cn/hasd/comp/CompApplication.java
@@ -0,0 +1,13 @@
+package cn.hasd.comp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hasd/comp/CompApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hasd.comp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
