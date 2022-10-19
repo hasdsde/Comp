319bfc4ea563b6205f551ec91086328a8964f253
hasdsde <1259305706@qq.com>
2022-10-19 16:58:53 +0800

    测试接口

--- src/main/java/cn/hasd/comp/controller/StudentController.java
+++ src/main/java/cn/hasd/comp/controller/StudentController.java
@@ -1,11 +1,17 @@
 package cn.hasd.comp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.hasd.comp.entity.Student;
+import cn.hasd.comp.mapper.StudentMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author hasd
@@ -14,5 +20,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+    @Resource
+    StudentMapper studentMapper;
 
+    @GetMapping("/comp1")
+    public List<Student> CompA() {
+        return studentMapper.compA();
+    }
 }
--- src/main/java/cn/hasd/comp/mapper/StudentMapper.java
+++ src/main/java/cn/hasd/comp/mapper/StudentMapper.java
@@ -2,15 +2,20 @@ package cn.hasd.comp.mapper;
 
 import cn.hasd.comp.entity.Student;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author hasd
  * @since 2022-10-19
  */
+@Mapper
 public interface StudentMapper extends BaseMapper<Student> {
 
+    List<Student> compA();
 }
